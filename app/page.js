301d0ad3e7615cd19c51678ad561a21814e2e209
Mayank Kathuria <mayank4kathuria@gmail.com>
2024-11-20 16:13:46 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,52 +1,34 @@
+"use client"
+import React, { useEffect } from "react";
 import Image from "next/image";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+// let DotLottieReact;
+// import appData from '@/app/assets/web-build.lottie';
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  // useEffect(() => {
+  //   DotLottieReact = import('@lottiefiles/dotlottie-react');
+  // })
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-r from-slate-900 via-slate-950 to-slate-900">
+      <main className="flex gap-20 row-start-2 items-center sm:items-start">
+
+        <div className="flex flex-1" >
+
+          <DotLottieReact
+            src="/assets/web-build.lottie"
+            loop
+            autoplay
+          />
+        </div>
+
+        <div className="flex flex-1 flex-col self-center" >
+          <p className="text-8xl" >Looking to go digital <span className="text-indigo-500" >?</span></p>
+          <p className="text-6xl mt-16" >Start with a website</p>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
@@ -58,12 +40,12 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
+            src="/globe.svg"
+            alt="Globe icon"
             width={16}
             height={16}
           />
-          Learn
+          Build
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -78,7 +60,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Check
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -88,12 +70,12 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
+            src="/file.svg"
+            alt="File icon"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Docs
         </a>
       </footer>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-20deg)' },
+          '50%': { transform: 'rotate(20deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      }
     },
   },
   plugins: [],
